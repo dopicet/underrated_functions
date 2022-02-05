3a45272b5ae1461a05d001b676a5c099ff290908
dopicet <93188998+dopicet@users.noreply.github.com>
2022-02-05 18:25:32 +0530

    Add files via upload

---
+++ underrated_functions.py
@@ -0,0 +1,9 @@
+"""vars"""
+class HomeExpensive():
+    def __init__(self):
+        self.bill = 10
+        self.domain = 15
+        self.petty_expensive = 20
+
+home_expensive = HomeExpensive()
+print(vars(home_expensive))
