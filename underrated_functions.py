b6584d232f183019b76b7199146b3784852538e4
dopicet <93188998+dopicet@users.noreply.github.com>
2022-02-05 18:26:01 +0530

    Update underrated_functions.py

--- underrated_functions.py
+++ underrated_functions.py
@@ -1,8 +1,23 @@
-"""vars"""
+# eval
+evaluate_string = '(y*(x+1)) + (y*(x+2))'
+x = 5
+y = 2
+expression = eval(evaluate_string)
+print(expression)
+
+# any
+lst = [0]
+print(any(lst))
+
+lst = [1,0]
+print(any(lst))
+
+# vars
 class HomeExpensive():
+
     def __init__(self):
-        self.bill = 10
-        self.domain = 15
+        self.electricity_bill = 10
+        self.domain_charges = 15
         self.petty_expensive = 20
 
 home_expensive = HomeExpensive()
